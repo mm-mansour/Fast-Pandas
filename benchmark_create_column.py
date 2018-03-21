e1a7e5ba12829a140e85d71f42f6b1798653652d
raybuhr <rebuhr@gmail.com>
2018-03-20 22:09:11 -0500

    create column using series np.values

--- benchmark_create_column.py
+++ benchmark_create_column.py
@@ -5,13 +5,17 @@ def regular(df):
     df["E"] = df["A"] * df["B"] + df["C"]
 
 
+def use_np_values(df):
+    df["E"] = df["A"].values * df["B"].values + df["C"].values
+
+
 def eval_method(df):
     df.eval("E = A * B + C", inplace=True)
 
 
 params = {
     "df_generator": 'pd.DataFrame(np.random.randint(1, df_size, (df_size, 4)), columns=list("ABCD"))',
-    "functions_to_evaluate": [regular, eval_method],
+    "functions_to_evaluate": [regular, use_np_values, eval_method],
     "title": "Benchmark for column creation",
 }
 
